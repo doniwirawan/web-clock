378810f432a8814c774aa8b7fc9e98090853124f
doniwirawan <doniwirawan166@gmail.com>
2020-11-11 09:38:40 +0800

    adding quote by fetch

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ const displayTime = () => {
     $(".date").text(moment().format("LL."));
     $(".day").text(moment().format("dddd."));
 };
- 
+
 const updateTime = () => {
     displayTime();
     setTimeout(updateTime, 1000)
 };
- 
+
+fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        document.querySelector('.button').addEventListener('click', function () {
+            const root = document.querySelector('.quote-area');
+            const random = Math.floor(Math.random() * data.length)
+            root.innerHTML = `
+                <p>"${data[random].text}"</p>
+                <p>-- ${data[random].author} --</p>
+
+
+    `
+
+        })
+
+
+    });
+
+
 updateTime();
